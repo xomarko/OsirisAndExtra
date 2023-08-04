5b5cc512c3614793d4bd511223c1fe06a0102a45
Marko <115811042+xomarko@users.noreply.github.com>
2023-08-04 15:51:23 +0800

    Fix crash when in game

--- Source/SDK/MoveHelper.h
+++ Source/SDK/MoveHelper.h
@@ -6,6 +6,6 @@ class Entity;
 
 class MoveHelper {
 public:
-    VIRTUAL_METHOD(void, setHost, 1, (Entity* host), (this, host))
+    VIRTUAL_METHOD(void, setHost, 1, (Entity* host), (host))
     VIRTUAL_METHOD(void, processImpacts, 4, (), (this))
 };
