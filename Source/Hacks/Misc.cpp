5732b91fc2cb7fdc5e0f35bb23185a8bdbb3691d
Marko <115811042+xomarko@users.noreply.github.com>
2023-08-23 00:19:10 +0800

    Extract isPlayerReported() function from Misc::runReportbot()

--- Source/Hacks/Misc.cpp
+++ Source/Hacks/Misc.cpp
@@ -2468,6 +2468,11 @@ static int reportbotRound;
     return report;
 }
 
+[[nodiscard]] static bool isPlayerReported(std::uint64_t xuid)
+{
+    return std::ranges::find(std::as_const(reportedPlayers), xuid) != reportedPlayers.cend();
+}
+
 void Misc::runReportbot() noexcept
 {
     if (!config->misc.reportbot.enabled)
@@ -2497,7 +2502,7 @@ void Misc::runReportbot() noexcept
         if (!interfaces->engine->getPlayerInfo(i, playerInfo))
             continue;
 
-        if (playerInfo.fakeplayer || std::find(reportedPlayers.cbegin(), reportedPlayers.cend(), playerInfo.xuid) != reportedPlayers.cend())
+        if (playerInfo.fakeplayer || isPlayerReported(playerInfo.xuid))
             continue;
 
         if (const auto report = generateReportString(); !report.empty()) {
