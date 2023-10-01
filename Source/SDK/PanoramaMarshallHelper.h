d15547f0c7d5e69da164f174fde86204df66df61
Marko <115811042+xomarko@users.noreply.github.com>
2023-10-01 23:26:07 +0800

    Add PanoramaMarshallHelper class

--- Source/SDK/PanoramaMarshallHelper.h
+++ Source/SDK/PanoramaMarshallHelper.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include "VirtualMethod.h"
+
+class PanoramaMarshallHelper {
+public:
+    VIRTUAL_METHOD(void, setResult, 14, (void* params, int result), (this, params, result))
+};
